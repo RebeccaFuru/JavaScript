5c8e745b42799f8ffd0f280589f0427a26e4ff1c
RebeccaFuru <rebecca.furu@gmail.com>
2017-11-21 10:44:46 +0100

    nya grejer

---
+++ data.js
@@ -0,0 +1,10 @@
+let movieData = {
+ title: "Lejonkungen",
+ description: "Det är en stor dag i Lejonriket när lejonungen Simba föds. Han ska en dag ta över sin pappa Mufasas roll och förvandlas från lejonungen till Lejonkungen! Alla jublar utom Scar, Mufasas lömske bror, som själv skulle vilja sitta på tronen." + 
+ "Tiden går och den nyfikne Simba får lära sig mer och mer om sitt blivande rike. Han följer sin pappa, får kloka råd av babianen Rafiki och busar med kompisen Nala." + 
+ "Men en dag händer det som inte får hända. Mufasa är plötsligt borta – och innan någon vet ordet av har Scar tagit över tronen!" + 
+ "Simba tvingas fly och långt borta från sin familj träffar han lustigkurren Timon och det oborstade vårtsvinet Pumbaa, som blir hans nya vänner." + 
+ "Men en dag återvänder Simba till Lejonriket för att återta sin plats som den rätte Lejonkungen…",
+ actorList: ["Simba", "Scar", "Timon", "Pumba"],
+ picURL: "http://dizw242ufxqut.cloudfront.net/images/product/movie/dvd/image1/disney_klassiker_32_lejonkungen-955527-.jpg"
+};
